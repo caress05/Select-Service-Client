b794cbd0a9dd3bdff854bf589b95d0dc28ec2ca8
caress05 <caress_roche@yahoo.com>
2016-02-02 12:16:49 -0800

    CommitSelectServiceClient

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownListShowArtists" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownListShowArtists_SelectedIndexChanged"></asp:DropDownList>
+        <asp:GridView ID="GridViewShowArtists" runat="server"></asp:GridView>
+        <asp:DropDownList ID="DropDownListShowVenues" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownListShowVenues_SelectedIndexChanged1"></asp:DropDownList>
+        <asp:GridView ID="GridViewShowVenues" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerServiceReference.VenueShowServiceClient db = new ShowTrackerServiceReference.VenueShowServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadDropDownSA();
+            LoadDropDownSV();
+        }
+    }
+    protected void DropDownListShowArtists_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FillGridSA();
+    }
+
+    protected void LoadDropDownSA()
+    {
+        string[] shows = db.GetArtists();
+        DropDownListShowArtists.DataSource = shows;
+        DropDownListShowArtists.DataBind();
+        DropDownListShowArtists.Items.Insert(0, "Choose an Artist");
+    }
+
+    protected void FillGridSA()
+    {
+        string artist = DropDownListShowArtists.SelectedItem.Text;
+        ShowTrackerServiceReference.ShowLite[] artists = db.GetShowArtists(artist);
+        GridViewShowArtists.DataSource = artists;
+        GridViewShowArtists.DataBind();
+    }
+
+    protected void DropDownListShowVenues_SelectedIndexChanged1(object sender, EventArgs e)
+    {
+        FillGridSV();
+    }
+
+    protected void LoadDropDownSV()
+    {
+        string[] shows = db.GetVenues();
+        DropDownListShowVenues.DataSource = shows;
+        DropDownListShowVenues.DataBind();
+        DropDownListShowVenues.Items.Insert(0, "Choose a Venue");
+    }
+
+    protected void FillGridSV()
+    {
+        string venue = DropDownListShowVenues.SelectedItem.Text;
+        ShowTrackerServiceReference.ShowLite1[] venues = db.GetShowVenues(venue);
+        GridViewShowVenues.DataSource = venues;
+        GridViewShowVenues.DataBind();
+    }
+
+}
